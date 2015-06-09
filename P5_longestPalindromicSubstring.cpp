77d8b68a90703fedf7a3a5eae1da104c02709c37
XieChang <toxic.xc@sjtu.edu.cn>
2015-06-09 18:28:33 +0800

    Update P5_longestPalindromicSubstring.cpp

--- P5_longestPalindromicSubstring.cpp
+++ P5_longestPalindromicSubstring.cpp
@@ -70,3 +70,52 @@ public:
       return longest;
     }
 };
+
+/**
+ *  Manacher’s Algorithm
+ *  Time O(n) Space O(n)
+ * 
+ **/
+ 
+class Solution {
+public:
+    static string preProcess(string s) {
+        if(s.length() == 0) {
+            return "^$";
+        }
+        string ret = "^";
+        for(int i = 0; i < s.length(); i++) {
+            ret += "#" + s.substr(i,1);
+        }
+        ret += "#$";
+        return ret;
+    }
+    string longestPalindrome(string s) {
+        string T = preProcess(s);
+        int n = T.length();
+        int C = 0, R = 0;
+        int* P = new int[n];
+        for(int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * C - i;
+            P[i] = R > i ? min(R - i, P[i_mirror]) : 0;
+            while(T[i + 1 + P[i]] ==  T[i - 1 - P[i]]) {
+                P[i]++;
+            }
+            if(P[i] + i > R) {
+                R = i + P[i];
+                C = i;
+            }
+        }
+        int maxLen = 0;
+        int center = 0;
+        for(int i = 1; i < n - 1; i++) {
+            if(P[i] > maxLen) {
+                maxLen = P[i];
+                center = i;
+            }
+        }
+        delete [] P;
+        P = nullptr;
+        return s.substr((center - maxLen - 1) / 2, maxLen);
+    }
+};
