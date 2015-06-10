cbe71647e3c789a1a805e6f1f735949665b7ae66
XieChang <toxic.xc@sjtu.edu.cn>
2015-06-10 18:12:13 +0800

    Update P6_ZigZagConversion.cpp

--- P6_ZigZagConversion.cpp
+++ P6_ZigZagConversion.cpp
@@ -1,3 +1,9 @@
+/**
+ * Solution 1
+ * Time : 32ms
+ * 
+ **/
+ 
 class Solution {
 public:
 	string convert(string s, int numRows) {
@@ -12,6 +18,32 @@ public:
 			else {
 				strArr[numRows * 2 - 2 - pos] += s[i];
 			}
+		}
+		string result = "";
+		for (int i = 0; i < numRows; i++) {
+			result += strArr[i];
+		}
+		return result;
+	}
+};
+
+/**
+ *  Solution 2
+ *  Time 20ms
+ * 
+ **/
+ 
+class Solution {
+public:
+	string convert(string s, int numRows) {
+		if (numRows == 1) return s;
+		string* strArr = new string[numRows];
+		int n = s.length();
+		int i = 0;
+		int gap = numRows - 2;
+		while (i < n) {
+			for (int j = 0; i < n && j < numRows; j++) strArr[j] += s[i++];
+			for (int j = gap; i < n && j > 0; j--) strArr[j] += s[i++];
 		}
 		string result = "";
 		for (int i = 0; i < numRows; i++) {
