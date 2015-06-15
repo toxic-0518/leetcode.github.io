eebd7e4a13bd2babdec1f110a3506d9372363eb3
XieChang <toxic.xc@sjtu.edu.cn>
2015-06-15 14:59:57 +0800

    Create P7_ReverseInteger.cpp

---
+++ P7_ReverseInteger.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	int reverse(int x) {
+		int result = 0;
+		while (true) {
+			result += (x % 10);
+			x /= 10;
+			if (!x) break;
+			if (result > INT_MAX / 10 || result  < INT_MIN / 10) return 0;
+			result *= 10;
+		}
+		return result;
+	}
+};
