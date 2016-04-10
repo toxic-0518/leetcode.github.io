82194de7c1b111cfecad9e62bf03596d7152fdc8
XieChang <toxic.xc@sjtu.edu.cn>
2016-04-10 19:11:10 +0800

    Update P319_BulbSwitcher.cpp

--- P319_BulbSwitcher.cpp
+++ P319_BulbSwitcher.cpp
@@ -1,11 +1,11 @@
-// We know that if the k-th bulb has n different factors
+// We know that if the k-th bulb has n different factors,
 // it will be toggled n times. if n is odd, the bulb will
 // be turned on finally or otherwise be turned off.
 // We also know that if p is a factor of k, then k / p must
 // also be a factor of p. Usually, n should be even because 
 // the factor p and k / p always appear pairwisely, except for 
 // one situation, when p = k / p. In this case, p and k / p 
-// are the same number, which indicates that k is squre of p.
+// are the same numbers, which indicates that k is squre of p.
 // So we can draw a conclusion that if k is a squre number,
 // then the bulb will be keeped on, or it will be turned off.
 
