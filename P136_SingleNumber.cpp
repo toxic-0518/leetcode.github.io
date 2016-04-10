bfa5df8e4cd60f3751beabbf61dcae7aa13fe20c
XieChang <toxic.xc@sjtu.edu.cn>
2016-04-10 17:54:36 +0800

    Create P136_SingleNumber.cpp

---
+++ P136_SingleNumber.cpp
@@ -0,0 +1,10 @@
+//O(n) solution
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = nums[0];
+        // xor bit operation
+        for(int i = 1; i < nums.size(); i++) result ^=nums[i];
+        return result;
+    }
+};
