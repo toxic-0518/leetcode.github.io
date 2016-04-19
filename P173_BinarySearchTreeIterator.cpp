2c6f196bf29401590efa603dc7a75650b9df7084
XieChang <toxic.xc@sjtu.edu.cn>
2016-04-19 22:59:35 +0800

    Create P173_BinarySearchTreeIterator.cpp

---
+++ P173_BinarySearchTreeIterator.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        p = root;
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return (p || !s.empty());  
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        while(p) {
+            s.push(p);
+            p = p->left;
+        }
+        p = s.top();
+        s.pop();
+        int val = p->val;
+        p = p->right;
+        return val;
+    }
+    
+private:
+    stack<TreeNode*> s;
+    TreeNode *p;
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
